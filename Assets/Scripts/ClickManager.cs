0f853a60dbad3fc0e1824a7309a7954bc8215c40
Opelsi <opelsisq@gmail.com>
2020-03-24 16:35:58 +0300

    Added the first levels

--- Assets/Scripts/ClickManager.cs
+++ Assets/Scripts/ClickManager.cs
@@ -5,26 +5,94 @@ using UnityEngine;
 public class ClickManager : MonoBehaviour
 {
 	Piece selectedPiece = null;
+	Piece connectedPiece = null;
+	
+	int findWinnerPoint(int distance, int strengthA, int strengthB, int pointA, int pointB)
+	{
+		int result = 0;
+		if (distance % 2 != 0)
+		{
+			if (strengthA > strengthB)
+			{
+				result = pointA + distance / 2;
+			}
+			else
+			{
+				result = pointB - distance/2;
+			}
+		}
+		else
+		{
+			result = pointA + distance / 2;
+		}
+		return result;
+	}
 	// Update is called once per frame
 	void Update()
-    {
+	{
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 		if (Input.GetMouseButtonDown(0))
 		{
-			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
-
 			RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
 			if (hit.collider != null)
 			{
 				if (hit.collider.tag == "Triangle")
 					selectedPiece = hit.collider.GetComponent<Piece>();
-				if (selectedPiece != null) selectedPiece.onSelect();
+				if (selectedPiece != null)
+					selectedPiece.onSelect();
 			}
 		}
-		if (Input.GetMouseButtonUp(0))
+		if (selectedPiece != null && Input.GetMouseButtonUp(0))
 		{
-			if(selectedPiece!=null)
+			RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+			if (hit.collider != null && hit.collider.gameObject!=selectedPiece.gameObject)
+			{
+				if ((hit.collider.tag == "Triangle" || hit.collider.tag == "Square")&&(Mathf.Abs(selectedPiece.transform.position.x-hit.collider.transform.position.x)<0.01f)|| (Mathf.Abs(selectedPiece.transform.position.y - hit.collider.transform.position.y)<0.01f))
+					connectedPiece = hit.collider.GetComponent<Piece>();
+				if (connectedPiece != null)
+				{
+					//connection process
+					int strengthA = selectedPiece.strength;
+					int strengthB = connectedPiece.strength;
+					connectedPiece.onConnect(selectedPiece, strengthA);
+					if (selectedPiece.tag!=connectedPiece.tag)
+						selectedPiece.onConnect(connectedPiece, strengthB);
+					//after connection
+					if (selectedPiece.tag == connectedPiece.tag)
+					{
+						selectedPiece.isLost = true;
+						selectedPiece.moveTo(connectedPiece);
+						connectedPiece.moveTo(connectedPiece);
+					}
+					else
+					{
+						connectedPiece.isLost = strengthA >= strengthB;
+						selectedPiece.isLost = strengthB >= strengthA;
+						int distCol = connectedPiece.col - selectedPiece.col;
+						int distRow = connectedPiece.row - selectedPiece.row;
+						int winnerPoint = 0;
+						Vector2 newPoint = new Vector2(0, 0);
+						if (distCol != 0)
+						{
+							winnerPoint = findWinnerPoint(distCol, strengthA, strengthB, selectedPiece.col, connectedPiece.col);
+							newPoint = new Vector2(winnerPoint, selectedPiece.row);
+						}
+						if (distRow != 0)
+						{
+							winnerPoint = findWinnerPoint(distRow, strengthA, strengthB, selectedPiece.row, connectedPiece.row);
+							newPoint = new Vector2(selectedPiece.col,winnerPoint );
+						}
+						selectedPiece.moveTo(newPoint);
+						connectedPiece.moveTo(newPoint);
+					}
+				}
+				connectedPiece = null;
+			}
+			if (selectedPiece!=null && connectedPiece == null)
+			{
 				selectedPiece.onDeselect();
+			}
 			selectedPiece = null;
 		}
 	}
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-	public Sprite sprite_1;
-	public Sprite sprite_2;
-	public Sprite sprite_3;
-	public SpriteRenderer sprite_selected;
-	public SpriteRenderer currentSprite;
+	public Sprite sprite_1, sprite_2, sprite_3;
+	public SpriteRenderer sprite_selected, currentSprite, whiteLine;
 	public int strength = 1;
-	private bool isSelected = false;
+	private bool isSelected = false, isConnected = false, isMoving = false;
+	public bool isLost = false;
+	public int col = 0, row = 0;
+	private Vector2 target = new Vector2(0, 0);
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
+	{
+		row = Mathf.RoundToInt(3 - transform.position.y);
+		col = Mathf.RoundToInt(3 + transform.position.x);
+	}
 
     // Update is called once per frame
     void Update()
@@ -33,16 +34,64 @@ public class Piece : MonoBehaviour
 				currentSprite.sprite = sprite_3;
 				break;
 		}
-		sprite_selected.enabled = isSelected;
+		sprite_selected.enabled = isSelected||isConnected||isMoving;
+		if (isSelected)
+		{
+			Vector3 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector3 distance = targetPos - transform.position; distance.z = 0;
+
+			if (Mathf.Abs(distance.x) > Mathf.Abs(distance.y)) distance -= new Vector3(0, distance.y, 0);
+			else distance -= new Vector3(distance.x, 0, 0);
+			whiteLine.transform.position = transform.position + distance / 2;
+			float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
+			whiteLine.transform.rotation = Quaternion.AngleAxis(angle,Vector3.forward);
+			whiteLine.transform.localScale = new Vector3(distance.magnitude, 1, 1);
+		}
+		if (isMoving)
+		{
+			Vector3 startPoint = transform.position;
+			Vector3 endPoint = new Vector3(target.x - 3, 3 - target.y, 0);
+			transform.position = Vector3.Lerp(startPoint, endPoint,  Time.deltaTime*10.0f);
+			if (Mathf.Abs(transform.position.x - endPoint.x)<0.01f&& Mathf.Abs(transform.position.y - endPoint.y)<0.01f)
+			{
+				isMoving = false;
+				row = Mathf.RoundToInt(3 - transform.position.y);
+				col = Mathf.RoundToInt(3 + transform.position.x);
+				Debug.Log((3 - transform.position.y) + " " + (3 + transform.position.x));
+				if(isLost)Destroy(gameObject);
+				isConnected = false;
+			}
+		}
 	}
 	public void onSelect()
 	{
-		Debug.Log("onSelect");
 		isSelected = true;
 	}
 	public void onDeselect()
 	{
-		Debug.Log("onDeselect");
 		isSelected = false;
+		whiteLine.transform.position = transform.position + new Vector3(0,0,1);
+		whiteLine.transform.localScale = new Vector3(0, 1, 1);
+	}
+	public void onConnect(Piece otherPiece,int tmpStrength)
+	{
+		isConnected = true;
+		if (otherPiece.tag == tag) strength += tmpStrength;
+		else strength -= tmpStrength;
+		if (strength < 0) strength = 0;
+	}
+	public void moveTo(float x, float y )
+	{
+		isMoving = true;
+		target = new Vector2(x, y);
+	}
+	public void moveTo(Piece otherPiece )
+	{
+		moveTo(otherPiece.col, otherPiece.row);
+	}
+	public void moveTo( Vector2 newPoint)
+	{
+		isMoving = true;
+		target = newPoint;
 	}
 }
